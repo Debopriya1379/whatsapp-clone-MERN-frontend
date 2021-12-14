4cae69f698be87ea56012c45f2d82518180a1574
Debopriya Das <debopriyadas721@gmail.com>
2021-12-15 00:30:24 +0700

    connected to backend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
+import { useEffect,useState } from 'react';
+import Pusher from 'pusher-js';
 import './App.css';
 import Sidebar from './components/Sidebar';
 import ChatComponent from './components/ChatComponent';
+import Axios from './Axios'   //from Axios.js
 
 function App() {
+  
+  const[messages,setMessages] = useState([]);
+
+  useEffect(()=>{
+    Axios.get('/messages/sync')
+    .then(response =>{
+      setMessages(response.data)
+    })
+  },[])
+
+  useEffect(() => {
+    //once
+    const pusher = new Pusher('38b182dc4068dfda3e2b', {
+      cluster: 'ap2'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (newMessage)=> {
+      // alert(JSON.stringify(newMessage));
+      setMessages([...messages,newMessage])
+    });
+
+    return ()=>{
+      channel.unbind_all();
+      channel.unsubscribe();
+    }
+
+  }, [messages])
+
+  console.log(messages)
+
+
   return (
     <div className="app">
       <div className="appBody">
         <Sidebar/>
-        <ChatComponent/>
+        <ChatComponent messages={messages}/>
       </div>
     </div>
   );
--- src/components/ChatComponent.js
+++ src/components/ChatComponent.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import {React,useState} from 'react'
 import '../components/chatComponent.css'
+import Axios from '../Axios.js'
 
 import {Avatar, IconButton} from '@mui/material'
 import MoreVertIcon from '@mui/icons-material/MoreVert';
@@ -8,7 +9,21 @@ import AttachFile from '@mui/icons-material/AttachFile';
 import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
 import MicIcon from '@mui/icons-material/Mic';
 
-function ChatComponent() {
+function ChatComponent({messages}) {
+
+    const[input,setInput] = useState('')
+
+    const sendMessage = async(e)=>{
+        e.preventDefault();
+        await Axios.post('/messages/new',{
+            "message": input,
+            "name": "demo",
+            "timestamp": " demo time",
+            "received": true
+        })
+        setInput('')
+    }
+
     return (
         <div className="Chat">
 
@@ -32,44 +47,28 @@ function ChatComponent() {
             </div>
 
             <div className="Chat_Body">
-                <p className="Chat_Message">
-                    <span className="Chat_Name">Sonny</span>
-                    This is a meseage   {/*p tag main content*/}
-                    <span className="Chat_Timestamp">
-                        {new Date().toUTCString()}
-                    </span>
-                </p>
 
-                <p className="Chat_Message">
-                    <span className="Chat_Name">Sonny</span>
-                    This is a meseage   {/*p tag main content*/}
-                    <span className="Chat_Timestamp">
-                        {new Date().toUTCString()}
-                    </span>
-                </p>
-
-                <p className="Chat_Message Chat_Receiver">
-                    <span className="Chat_Name">Sonny</span>
-                    This is a meseage   {/*p tag main content*/}
-                    <span className="Chat_Timestamp">
-                        {new Date().toUTCString()}
-                    </span>
-                </p>
-
-                <p className="Chat_Message">
-                    <span className="Chat_Name">Sonny</span>
-                    This is a meseage   {/*p tag main content*/}
-                    <span className="Chat_Timestamp">
-                        {new Date().toUTCString()}
-                    </span>
-                </p>
+                {
+                    messages.map((message)=>{
+                        return(
+                            <p className={`Chat_Message ${message.received && 'Chat_Receiver'}`}>
+                        <span className="Chat_Name">{message.name}</span>
+                        {message.message}   {/*p tag main content*/}
+                        <span className="Chat_Timestamp">
+                            {/* {new Date().toUTCString()} */}
+                            {message.timestamp}
+                        </span>
+                        </p>
+                        )
+                    })
+                }
             </div>
 
             <div className="Chat_Footer">
                 <InsertEmoticonIcon/>
                 <form>
-                    <input type="text" placeholder="Type a message" />
-                    <button type="submit">Send a message</button>
+                    <input value={input} onChange={e=> setInput(e.target.value)} type="text" placeholder="Type a message" />
+                    <button onClick={sendMessage} type="submit">Send a message</button>                
                 </form>
                 <MicIcon/>
             </div>
